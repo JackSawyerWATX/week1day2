21f3a673d479c5e3f083707bdf9ae20d2d51ece2
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-09-12 19:42:14 -0700

    reformat

--- week1day2.js
+++ week1day2.js
@@ -1,14 +1,9 @@
-  function getTotal(arrayOfNumbers) {
-    
-    var sum = arrayOfNumbers[0];
-      
-    for(var i=0; i<arrayOfNumbers.length; i++) {
-      sum += arrayOfNumbers[i];
-      console.log("the current sum is: " + sum); 
-    }
-      
-    console.log("the total is: " + sum);
-      
+function getTotal(arrayOfNumbers) {
+  var sum = arrayOfNumbers[0];
+  for (var i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+    console.log("the current sum is: " + sum);
   }
-      
-  getTotal([3, 6, 9]);
+  console.log("the total is: " + sum);
+}
+getTotal([3, 6, 9]);
